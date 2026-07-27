f61259ff66c343b32b8f90e43d2c3f823f2ddca8
Hanbo Guo <ghbhanbo@gmail.com>
2026-07-27 00:43:07 +0000

    feat: add GitHub activity stats section
    - Replace broken vercel github-readme-stats with self-hosted jstrieb/github-stats
    - Overview and languages cards served from generated branch

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Container } from '@chakra-ui/react';
+import { Box, Container, Heading, Image, SimpleGrid } from '@chakra-ui/react';
 import { HeroSection } from '../components/HeroSection';
 import { SkillsSection } from '../components/SkillsSection';
 import { TimelineSection } from '../components/TimelineSection';
@@ -12,6 +12,27 @@ export const Home: React.FC = () => {
       <TimelineSection />
 
       <SkillsSection />
+
+      {/* GitHub Stats */}
+      <Box py={8}>
+        <Heading as="h2" size="lg" mb={6} letterSpacing="tight" fontWeight="bold">
+          GitHub Activity
+        </Heading>
+        <SimpleGrid columns={{ base: 1, md: 2 }} spacing={4}>
+          <Image
+            src="https://raw.githubusercontent.com/Haannbboo/github-stats/generated/overview.svg"
+            alt="GitHub overview"
+            w="full"
+            borderRadius="xl"
+          />
+          <Image
+            src="https://raw.githubusercontent.com/Haannbboo/github-stats/generated/languages.svg"
+            alt="GitHub languages"
+            w="full"
+            borderRadius="xl"
+          />
+        </SimpleGrid>
+      </Box>
     </Container>
   );
 };
